38e09c8bb080570fbe8c334e14f89f27e799f79f
aedanmc <mccallaedan@gmail.com>
2022-09-02 19:19:47 -0700

    Add empty files for initial components/pages; implement initial version of NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function NavBar() {
+    return(
+        <div>
+            <div>
+                <Link to="/">
+                    Home
+                </Link>
+            </div>
+            <div>
+                <Link to="/about">
+                    About
+                </Link>
+            </div>
+            <div>
+                <Link to="/projects">
+                    Projects
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
