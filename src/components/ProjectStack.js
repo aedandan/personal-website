8ef8ae511145ae34346e38b83dbd4654a68927f2
aedanmc <mccallaedan@gmail.com>
2022-09-05 17:24:24 -0700

    Add stub code for rendering ProjectStack

--- src/components/ProjectStack.js
+++ src/components/ProjectStack.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-function ProjectStack(projectTechList) {
+function ProjectStack(projectLangList) {
     // TODO: Process project data served from somewhere
 
 
-    // TODO: Convert the array into a list or something 
+
+    // TODO: Convert the array into an unordered list
+
+    return(
+        // Convert the array of languages passed down from main projects page
+        // to an HTML list
+        projectLangList.map((language) => 
+        <li id={language} key={language}>{language}</li>)
+    );
 
 }
 
