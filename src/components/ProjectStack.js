62fcd54b4a4beaf17eda79d9a589b4eee174df36
aedanmc <mccallaedan@gmail.com>
2022-09-05 17:12:14 -0700

    Add TODOs to ProjectStack.js

---
+++ src/components/ProjectStack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectStack(projectTechList) {
+    // TODO: Process project data served from somewhere
+
+
+    // TODO: Convert the array into a list or something 
+
+}
+
+export default ProjectStack;
