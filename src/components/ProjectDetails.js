dc2096d83047d16e46b4ff7851827be601098f8a
aedanmc <mccallaedan@gmail.com>
2022-09-08 15:50:18 -0700

    Prep ProjectDetails with some inital CSS classes

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function ProjectDetails(projectName, shortDesc, projectImage, projectAlt, repoLink) {
     return(
-        <div>
-            <div>
-                <h3>{projectName}</h3>
+        <div className='project-tile-wrapper'>
+            <div className='project-name-wrapper'>
+                <h3 className='project-name'>{projectName}</h3>
             </div>
             <div>
                 <img src={projectImage} alt={projectAlt} />
             </div>
-            <div>
+            <div className='short-description'>
                 <p>{shortDesc}</p>
             </div>
             <div>
