a5f41e5837b56f28e073b12adac6f86822f3160c
aedanmc <mccallaedan@gmail.com>
2022-09-08 15:52:51 -0700

    Write initial version of HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,5 +2,16 @@ import React from 'react';
 
 function HomePage() {
     return(
+        <div>
+            <img 
+            src=''
+            alt='Aedan wearing'/>
+            <p>Welcome to my portfolio site! My name is Aedan, 
+                and I use they/them pronouns. See About for more!</p>
+            <img src=''
+            alt='Aedan' />
+        </div>
     );
 }
+
+export default HomePage;
