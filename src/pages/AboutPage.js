ffb1a2292a2d7f646092501108175d1e2cf1f5aa
aedanmc <mccallaedan@gmail.com>
2022-09-10 16:44:40 -0700

    Add draft images and classnames to AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,13 +3,19 @@ import React from 'react';
 function AboutPage() {
     return(
         <div>
+            <img src=''
+            alt='Aedan'/>
             <h2 className='about-header'>About Me</h2>
-            <p>
+            <p className='about-body'>
                 I am a software engineer interested in UI design and engineering.
                 My hobbies include martial arts, gaming, music, and climate justice organizing.
                 In the future, I hope to work on making everyday technology more accessible to 
                 as many people as possible. 
             </p>
+            <img src=''
+            alt='Aedan'/>
         </div>
     );
 }
+
+export default AboutPage;
