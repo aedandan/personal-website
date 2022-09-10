22c1750c5cd92fb6bf133cbbaa6710053b7a440a
aedanmc <mccallaedan@gmail.com>
2022-09-10 16:42:42 -0700

    Add draft version of AboutPage.js

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutPage() {
+    return(
+        <div>
+            <h2 className='about-header'>About Me</h2>
+            <p>
+                I am a software engineer interested in UI design and engineering.
+                My hobbies include martial arts, gaming, music, and climate justice organizing.
+                In the future, I hope to work on making everyday technology more accessible to 
+                as many people as possible. 
+            </p>
+        </div>
+    );
+}
