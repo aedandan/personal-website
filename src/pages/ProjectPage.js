9bcdc88a38b6efb2e30cc906318f996cc980047c
aedanmc <mccallaedan@gmail.com>
2022-09-15 12:42:42 -0700

    Add initial data rendering to ProjectPage

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import getStaticProjects from '../static/ProjectData';
+import ProjectStack from '../components/ProjectStack';
+import ProjectDetails from '../components/ProjectDetails';
 
-function ProjectPage() {
+function ProjectPage(staticMode) {
+    const [projects, setProjects] = React.useState([]);
 
+    React.useEffect(() => {
+        if (staticMode) {
+            setProjects(getStaticProjects);
+
+        } else {
+            // TODO: Dynamic project rendering on page load
+
+        }
+    }, [staticMode]);
+
+
+    return (
+        <>
+            {projects.map((project) => (
+                <React.Fragment key={project.id}>
+                    <ProjectStack projectLangList={project.stack} />
+                    <ProjectDetails projectName={project.name} 
+                    shortDesc={project.desc} /*projectImage={}*/ 
+                    projectAlt={project.alt} 
+                    repoLink={project.link} />
+                </React.Fragment>
+            ))}
+        </>
+        
+    );
 }
 
 export default ProjectPage;
