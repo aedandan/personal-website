c1854844ceaca85520e5b9d90a753fc9fd6c5795
aedanmc <mccallaedan@gmail.com>
2022-09-14 17:01:28 -0700

    Minor rewording for AboutPage.js blurb

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,7 +7,7 @@ function AboutPage() {
             alt='Aedan'/>
             <h2 className='about-header'>About Me</h2>
             <p className='about-body'>
-                I am a software engineer interested in UI design and engineering.
+                I am a software engineer interested in developing and designing user interfaces.
                 My hobbies include martial arts, gaming, music, and climate justice organizing.
                 In the future, I hope to work on making everyday technology more accessible to 
                 as many people as possible. 
