1f949d82807fc37ff691d71caafa238b26227d39
aedanmc <mccallaedan@gmail.com>
2022-09-15 13:38:40 -0700

    Modify static ProjectData to be less convoluted

--- src/static/ProjectData.js
+++ src/static/ProjectData.js
@@ -1,5 +1,5 @@
-const projectData = {
-    project1:{
+const projectData = [
+    {
         id: 1,
         name: 'Travelo-Hey!',
         desc: 'An app to help LGBTQ+ travelers find safe places abroad.',
@@ -7,7 +7,7 @@ const projectData = {
         link: 'https://github.com/aedanmc/travelo-hey',
         stack: ['Node.JS', 'React', 'JavaScript', 'CSS', 'HTML'],
     },
-    project2:{
+    {
         id: 2,
         name: 'Portfolio Site',
         desc: 'A lightweight website to showcase my work.',
@@ -15,39 +15,39 @@ const projectData = {
         link: '',
         stack: ['React', 'JavaScript', 'SCSS'],
     },
-    project3:{
+    {
         id: 3,
         name: 'Discord Event Scheduler',
-        desc: 'A tool to help members of a Discord server schedule gaming events',
+        desc: 'A tool to help members of a Discord server schedule gaming events.',
         alt: '',
         link: '',
         stack: ['Java'],
     },
-    project4:{
+    {
         id: 4,
         name: 'Campus Path Finder',
-        desc: '(UW Class Project) A full stack web app designed to help users navigate the UW campus',
+        desc: '(UW Class Project) A full stack web app designed to help users navigate the UW campus.',
         alt: 'Map of UW campus',
         link: '',
         stack: ['Java', 'TypeScript', 'React', 'CSS', 'HTML'],
     },
-    project5:{
+    {
         id: 5,
         name: 'Secure Network Search Engine',
-        desc: '(UW Class Project) A threaded application built to allow users to search a small directory of files remotely',
+        desc: '(UW Class Project) A threaded application built to allow users to search a small directory of files remotely.',
         alt: '',
         link: '',
         stack: ['C++', 'C', 'HTML'],
     },
-    project6:{
+    {
         id: 6,
         name: 'Disability Language Project',
-        desc: '(Non-technical role) A research project intended to help determine how best to refer to disabled people',
+        desc: '(Non-technical role) A research project intended to help determine how best to refer to disabled people.',
         alt: '',
         link: 'https://athersharif.me/documents/assets-2022-identity-first-vs-person-first.pdf',
         stack: ['JavaScript', 'React', 'EcmaScript'],
     }
-}
+]
 
 export default function getStaticProjects() {
     return projectData;
