83d900e186aaf68eb21fe479a87681fc1ac6d30d
aedanmc <mccallaedan@gmail.com>
2022-09-15 12:42:55 -0700

    Modify ProjectData to have project id

--- src/static/ProjectData.js
+++ src/static/ProjectData.js
@@ -1,5 +1,6 @@
 const projectData = {
     project1:{
+        id: 1,
         name: 'Travelo-Hey!',
         desc: 'An app to help LGBTQ+ travelers find safe places abroad.',
         alt: 'The Travelo-Hey! logo; the phrase Travelo-Hey! in stylish font',
@@ -7,6 +8,7 @@ const projectData = {
         stack: ['Node.JS', 'React', 'JavaScript', 'CSS', 'HTML'],
     },
     project2:{
+        id: 2,
         name: 'Portfolio Site',
         desc: 'A lightweight website to showcase my work.',
         alt: 'The website that this information is hosted on',
@@ -14,6 +16,7 @@ const projectData = {
         stack: ['React', 'JavaScript', 'SCSS'],
     },
     project3:{
+        id: 3,
         name: 'Discord Event Scheduler',
         desc: 'A tool to help members of a Discord server schedule gaming events',
         alt: '',
@@ -21,6 +24,7 @@ const projectData = {
         stack: ['Java'],
     },
     project4:{
+        id: 4,
         name: 'Campus Path Finder',
         desc: '(UW Class Project) A full stack web app designed to help users navigate the UW campus',
         alt: 'Map of UW campus',
@@ -28,6 +32,7 @@ const projectData = {
         stack: ['Java', 'TypeScript', 'React', 'CSS', 'HTML'],
     },
     project5:{
+        id: 5,
         name: 'Secure Network Search Engine',
         desc: '(UW Class Project) A threaded application built to allow users to search a small directory of files remotely',
         alt: '',
@@ -35,6 +40,7 @@ const projectData = {
         stack: ['C++', 'C', 'HTML'],
     },
     project6:{
+        id: 6,
         name: 'Disability Language Project',
         desc: '(Non-technical role) A research project intended to help determine how best to refer to disabled people',
         alt: '',
@@ -43,6 +49,6 @@ const projectData = {
     }
 }
 
-export default function getProjectData() {
+export default function getStaticProjects() {
     return projectData;
 }
