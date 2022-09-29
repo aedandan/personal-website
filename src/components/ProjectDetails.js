98345d018e5fa230e176b7283bee7385f2ba643a
aedanmc <mccallaedan@gmail.com>
2022-09-29 16:23:10 -0700

    Modify ProjectDetails to have less complexity

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-function ProjectDetails(projectName, shortDesc, projectImage, projectAlt, repoLink) {
+function ProjectDetails({ projectName, shortDesc, /*projectImage, projectAlt,*/ repoLink}) {
     return(
         <div className='project-tile-wrapper'>
             <div className='project-name-wrapper'>
-                <h3 className='project-name'>{projectName}</h3>
-            </div>
-            <img className='project-image' src={projectImage} alt={projectAlt} />
-            <div className='short-description'>
-                <p>{shortDesc}</p>
-            </div>
-            <div>
+                <h2 className='project-name'>{projectName}</h2>
                 <a href={repoLink}>
-                    Link to repository
+                    Project Link
                 </a>
             </div>
+            
+            {/* <img className='project-image' src={projectImage} alt={projectAlt} /> */}
+                <p className='short-description'>{shortDesc}</p>
+                
         </div>
     );
 }
