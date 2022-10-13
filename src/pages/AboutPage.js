91e3a81bede1c2cae1fe4b474bb8feb0a9cf602b
aedanmc <mccallaedan@gmail.com>
2022-10-12 18:35:24 -0700

    Modify about page structure for styling

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 function AboutPage() {
     return(
-        <div>
-            <img src=''
-            alt='Aedan'/>
-            <h2 className='about-header'>About Me</h2>
-            <p className='about-body'>
-                I am a software engineer interested in developing and designing user interfaces.
-                My hobbies include martial arts, gaming, music, and climate justice organizing.
-                In the future, I hope to work on making everyday technology more accessible to 
-                as many people as possible. 
-            </p>
-            <img src=''
-            alt='Aedan'/>
+        <div id='about-content'>
+            <div id='about-text'>
+                <h2 className='about-header'>About Me</h2>
+                <p className='about-body'>
+                    I am a software engineer interested in developing and designing user interfaces.
+                    My hobbies include martial arts, gaming, music, and climate justice organizing.
+                    In the future, I hope to work on making everyday technology more accessible to 
+                    as many people as possible. 
+                </p>
+            </div>
+            <div id='about-images'>
+                <img src=''
+                alt='Aedan'/>
+                <img src=''
+                alt='Aedan'/>
+            </div>
         </div>
     );
 }
