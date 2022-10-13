4a6e0ad6c9163abd57f6cd1160ba131491a4fca1
aedanmc <mccallaedan@gmail.com>
2022-10-12 18:35:06 -0700

    Add stylename for ProjectStack

--- src/components/ProjectStack.js
+++ src/components/ProjectStack.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
-function ProjectStack(projectLangList) {
-    // TODO: Process project data served from somewhere
-
-
-
-    // TODO: Convert the array into an unordered list
-
+function ProjectStack({ projectLangList }) {
     return(
         // Convert the array of languages passed down from main projects page
         // to an HTML list
-        <div>
+        <div className='project-tile-stack' >
             <ul>
                 {projectLangList.map((language) => 
                 <li id={language} key={language}>{language}</li>)}
             </ul>
         </div>
-        
     );
 
 }
